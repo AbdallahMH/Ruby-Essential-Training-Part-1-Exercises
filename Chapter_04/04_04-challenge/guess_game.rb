3f5b3a17e6dcfd2fff3aafa56a532bd5ef7dd110
Abdallah Mohammed <sofrex8@gmail.com>
2022-06-14 11:07:21 +0300

    guessing game challenge

--- Chapter_04/04_04-challenge/guess_game.rb
+++ Chapter_04/04_04-challenge/guess_game.rb
@@ -6,9 +6,36 @@ puts "
 ----------------------
 "
 
-print "What is your name? "
-response = gets.chomp
+puts "What is your name? "
+print "> "
+name = gets.chomp
 
-puts "Hello, #{response}! \nThis is the rules of the game:
-\n-Pick a random number and try to guess it 
--You have three guesses only"
+puts "Hello, #{name}!"
+puts "We are going to play a guessing game."
+puts "I will choose a random number between 1 and 10"
+puts "and you will have three chances to guess it."
+
+number = rand(11)
+puts "Now try to guess the number"
+
+MAX_GUESSES = 3
+
+1.upto(MAX_GUESSES) do |guess_num|
+  print "Guess #{guess_num}: "
+  guess = gets.chomp
+
+  if guess.to_i == number
+    puts
+    puts "Great guessing, #{name}"
+    puts "My number was #{number}."
+    break
+  else
+    puts "Sorry, that wasn't it."
+    if guess_num == MAX_GUESSES
+      puts "That was your last guess"
+      puts "My number was #{number}."
+    end
+  end
+end
+
+puts "\nGoodbye!"
